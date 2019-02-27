9b8d79982cc319a941ae808624165f737e120180
Bogdan Bujdea <bogdan@thewindev.net>
2019-02-28 00:36:19 +0200

    Initialized repo

---
+++ content/blog/my-work-diary/index.md
@@ -0,0 +1,9 @@
+---
+title: My work diary
+date: "2019-02-28T22:10:00.000Z"
+---
+I'm starting a new project today...a public work diary. I learn new things
+everyday and I always think they are too short for a blog post, so I'll
+be using this Gatsby site to publish short stories of things that I learn at work.
+Of course, my job requires that I keep my work confidential so I'll just share
+generic stuff, without disclosing projects or customers.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Work Diary`,
+    author: `Bogdan Bujdea`,
+    description: `A blog that showcases trivial stuff that I learn every day at work.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `thewindev`,
     },
   },
   plugins: [
